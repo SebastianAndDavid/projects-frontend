ad814f03657b95d63a77df2c835689f75eedcee8
David <Rigby335@hotmail.com>
2023-12-16 08:10:22 -0800

    feat: Projectcreate component form setup and fetching homeowners

--- src/components/projects/ProjectCreate.tsx
+++ src/components/projects/ProjectCreate.tsx
@@ -1,23 +1,52 @@
+import { useEffect, useState } from "react";
+import "./Projects.css";
+import { getAllHomeowners } from "../../fetch-utils";
+
 export default function ProjectCreate() {
+  const [homeownerArray, setHomeownerArray] = useState([]);
+  useEffect(() => {
+    const handleFetchAllHomeowners = async () => {
+      const data = await getAllHomeowners();
+      setHomeownerArray(data);
+    };
+    handleFetchAllHomeowners();
+  }, []);
   return (
     <div>
       <form>
-        Project Name
-        <input type="text" />
-        Deposit
-        <input type="text" />
-        Address Line 1
-        <input type="text" />
-        City
-        <input type="text" />
-        State
-        <input type="text" />
-        Apartment #
-        <input type="text" />
-        Zip
-        <input type="text" />
-        Description
-        <input type="text" />
+        <div className="input-container">
+          <section className="col-1">
+            Project Name
+            <input type="text" />
+            Description
+            <input type="text" />
+            Deposit
+            <input type="text" />
+            Homeowner
+            <select>
+              {homeownerArray.map((owner) => {
+                return (
+                  <option value="" key={owner.id}>
+                    {owner.first_name}
+                  </option>
+                );
+              })}
+            </select>
+          </section>
+          <section className="col-2">
+            Address Line 1
+            <input type="text" />
+            City
+            <input type="text" />
+            State
+            <input type="text" />
+            Apartment #
+            <input type="text" />
+            Zip
+            <input type="text" />
+          </section>
+        </div>
+        <button>Submit</button>
       </form>
     </div>
   );
