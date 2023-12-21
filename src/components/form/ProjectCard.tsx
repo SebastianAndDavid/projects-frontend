5a359e69dcb546718a528c72cec1aa765a3929e9
David <Rigby335@hotmail.com>
2023-12-21 10:10:34 -0800

    feat: displaying projects by client

--- src/components/form/ProjectCard.tsx
+++ src/components/form/ProjectCard.tsx
@@ -1,5 +1,20 @@
-import { ProjectCardProps } from "../projects/projects.interface";
+import { useEffect, useState } from "react";
+import {
+  ProjectCardProps,
+  ProjectSelect,
+} from "../projects/projects.interface";
+import { getAllProjectsByHomeownerId } from "../../fetch-utils";
 
 export default function ProjectCard({ client }: ProjectCardProps) {
+  const [projectsArray, setProjectsArray] = useState<ProjectSelect[]>([]);
+
+  useEffect(() => {
+    const handleFetchProject = async () => {
+      const data = await getAllProjectsByHomeownerId(client.id);
+      setProjectsArray(data);
+    };
+    handleFetchProject();
+  }, []);
+
   return <div>{client.first_name}</div>;
 }
