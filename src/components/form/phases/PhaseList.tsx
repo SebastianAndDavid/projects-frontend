84e3e914575e218a923831f57cb8caf16e059cc8
David <Rigby335@hotmail.com>
2024-01-06 15:59:55 -0800

    feat: styled phase table

--- src/components/form/phases/PhaseList.tsx
+++ src/components/form/phases/PhaseList.tsx
@@ -1,15 +1,16 @@
 import { usePhasesReadOnly } from "../../../context/projectsContext";
 import PhaseListItem from "./PhaseListItem";
+import "./PhaseList.css";
 
 export default function PhaseList() {
   const [phasesArrayReadOnly] = usePhasesReadOnly();
-
+  console.log(phasesArrayReadOnly);
   return (
-    <div>
-      <div>
+    <div className="phase-table">
+      <div className="phase-table-header">
         <h3>Phases</h3>
       </div>
-      <div>
+      <div className="phase-table-body">
         {phasesArrayReadOnly.map((phase) => {
           return <PhaseListItem key={phase.id} phase={phase} />;
         })}
--- src/components/form/phases/PhaseListItem.tsx
+++ src/components/form/phases/PhaseListItem.tsx
@@ -1,21 +1,13 @@
-import { useEffect } from "react";
 import { PhaseListItemProps } from "./phaseItem.interface";
-import { getAllReadOnlyTasksByMilestoneId } from "../../../fetch-utils";
 
 export default function PhaseListItem({ phase }: PhaseListItemProps) {
-  const handleFetchReadOnlyTasksByMilestoneId = async () => {
-    const data = await getAllReadOnlyTasksByMilestoneId();
-    console.log("data", data);
-  };
-  useEffect(() => {
-    handleFetchReadOnlyTasksByMilestoneId();
-  }, []);
-
   return (
-    <div>
-      <div>{phase.name}</div>
-      <div>Milestones: {phase.MilestonesReadOnly.length}</div>
-      <div>Tasks: </div>
+    <div className="phase-table-row-item">
+      <div className="phase-table-row-item-name">{phase.name}</div>
+      <div className="milestones">
+        Milestones: {phase.MilestonesReadOnly.length}
+      </div>
+      <div className="tasks">Tasks: </div>
     </div>
   );
 }
